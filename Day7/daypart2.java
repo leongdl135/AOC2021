4861c138eb7533147a322a44f802dfe1e6180fd9
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2021-12-07 13:23:15 +0800

    Add files via upload

---
+++ Day7/daypart2.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
+import java.util.Arrays;
+
+public class daypart2 {
+   public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+        //ArrayList<Integer> numbers = new ArrayList<>();
+
+        
+        String input = scanner.nextLine();
+        String[] parts = input.split(",");
+        int[] numbers = new int[parts.length];
+        for(int i = 0; i < parts.length; i++) {
+           numbers[i] = Integer.valueOf(parts[i]);
+        }
+
+        int total = 0;
+        for(int i = 0; i < numbers.length; i++) {
+           total += numbers[i];
+        }
+
+        int max = 0;
+        int min = 100;
+
+        for(int i = 0; i < numbers.length; i++) {
+           if (numbers[i] > max) {
+              max = numbers[i];
+           }
+           if (numbers[i] < min) {
+              min = numbers[i];
+           }
+        }
+        long leastFuelUsed = 100000000;
+        for(int i = min; i <= max; i++) {
+           long sum = 0;
+           for(int j = 0; j < numbers.length; j++) {
+               long num =  Math.abs(i - numbers[j]);
+               sum += ((num)*(num+1))/2;
+           }
+           if (sum < leastFuelUsed) {
+              leastFuelUsed = sum;
+           }
+        }
+
+        System.out.println("fuel: " + leastFuelUsed);
+        
+         
+        
+        
+
+
+   } 
+}
